b21ec13bacb1e06dbcf4d6120b762cb9129d07f7
Boardbot <84594756+Boardbot@users.noreply.github.com>
2021-07-05 11:49:22 -0400

    Update Boardbot's GRG GUI v2.lua

--- Boardbot's GRG GUI v2.lua
+++ Boardbot's GRG GUI v2.lua
@@ -16,12 +16,6 @@ when will he learn!
 
 
 
-
-
-game.CoreGui.FinityUI:Destroy()
-
-
-
 local vu = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:connect(function()
    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
