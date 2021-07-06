7c6bf80d9f69f57ca37a5cd8c973ba8d851f3798
Boardbot <84594756+Boardbot@users.noreply.github.com>
2021-07-06 14:22:23 -0400

    Update Boardbot's GRG GUI Source.lua

--- Boardbot's GRG GUI Source.lua
+++ Boardbot's GRG GUI Source.lua
@@ -1,1 +1,3 @@
+-- this is not the source. it just redirects to the actual source.
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Boardbot/grg/main/Boardbot's%20GRG%20GUI%20v2.lua"))()
